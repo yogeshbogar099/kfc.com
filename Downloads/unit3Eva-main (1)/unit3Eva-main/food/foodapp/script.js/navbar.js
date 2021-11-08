1084a165cce468631873098e1630495020d555c6
yogeshbogar099 <yogeshbogar099@gmail.com>
2021-11-08 17:28:53 +0530

    f

---
+++ Downloads/unit3Eva-main (1)/unit3Eva-main/food/foodapp/script.js/navbar.js
@@ -0,0 +1,14 @@
+function navbar () {
+    return `
+    
+
+     <div id="option">
+        <h3><a class="head1" >Menu</a><h3>
+         <h3><a class="head head-3" href="sign-up.html">Sign up</a></h3>
+         <h3><a class="head head-3" href="login.html">Login</a></h3>
+     </div>
+     
+   `
+}
+
+export default navbar
